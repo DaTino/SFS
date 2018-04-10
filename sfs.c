d0858b4cdfac52884940a8ad47fefd75b2f80aa8
apmq9b <apmq9b@delmar.umsl.edu>
2018-04-10 18:10:20 -0500

    Fleshed out the program, at this point it prints a file and counts the number of characters.

--- sfs.c
+++ sfs.c
@@ -6,6 +6,25 @@ int main( int argc, char **argv)
 {
     FILE* f_read;
 
+    f_read = fopen(argv[1], "r");
+    if (!f_read) {
+        printf("Uh oh. Something went wrong.\n");
+    }
+    else {
+        int c;
+        int numc;
+        while (!feof(f_read)){
+            if(fscanf(f_read, "%c", &c)){
+                printf("%c", c);
+                numc++;
+            }
+        }
+        
+        printf("Num chars: %d\n", numc);
+
+        fclose(f_read);
+    }
+
     
 
     return 0;
