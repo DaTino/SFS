24a283d7a9c875df44e099c402083c4e1ea74037
apmq9b <apmq9b@delmar.umsl.edu>
2018-04-12 18:18:46 -0500

    Still getting user input to work

--- sfs.c
+++ sfs.c
@@ -15,33 +15,47 @@ int main(int argc, char **argv)
 
     if (argc=1) {
         //make the same things but for stdio..
-        FILE* temp = freopen("temp.txt", "w+", stdout);
+       
+        FILE* temp = fopen("temp.txt", "w+");
+        char filename[] = "temp";
+        int input=0;
+
+        input = fgetc(temp);
+        whilea (input != EOF) {
+          input = fgetc(temp);
+        }
+
         lcount = countl(temp);
         ccount = countc(temp);
         wcount = countw(temp);
-        fclose(temp);
 
-    } 
-
-
-
-    FILE* f_read;
-    f_read = fopen(argv[1], "r");
-
-    if (!f_read) {
-        printf("Sommat fucked up.\n");
-    }
-    else {
-
-        lcount = countl(f_read);
-        ccount = countc(f_read);
-        wcount = countw(f_read);
-        
         printf("COUNTS: %d, %d, %d\n", ccount, lcount, wcount);
 
-    }
+        fclose(temp);
+        remove(filename);
 
-    fclose(f_read);
+    } 
+    else {
+
+
+        FILE* f_read;
+        f_read = fopen(argv[1], "r");
+
+        if (!f_read) {
+            printf("Sommat fucked up.\n");
+        }
+        else {
+
+            lcount = countl(f_read);
+            ccount = countc(f_read);
+            wcount = countw(f_read);
+        
+            printf("COUNTS: %d, %d, %d\n", ccount, lcount, wcount);
+
+        }   
+
+        fclose(f_read);
+    }
 
     return 0;
 }
